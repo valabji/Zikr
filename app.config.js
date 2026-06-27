71757da8656c70ed28eaba08bc7f03080e5de7db
valabji-wbhr <117309695+valabji-wbhr@users.noreply.github.com>
2026-06-27 03:56:43 +0300

    v1.1.19

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
     slug: "Zikr",
     privacy: "public",
     platforms: ["ios", "android", "web"],
-    version: "1.1.18",
+    version: "1.1.19",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "myapp",
@@ -42,7 +42,7 @@ export default {
     android: {
       package: "com.valabji.zikr",
       googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-      versionCode: 21,
+      versionCode: 22,
       adaptiveIcon: {
         foregroundImage: "./assets/images/splash_noname.png",
         monochromeImage: "./assets/images/logo_noname.png",
